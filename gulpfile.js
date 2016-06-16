9f5cbc50a8694cfef6f23e56edc73791c9993f04
dnomak <me@dnomak.com>
2016-06-16 17:41:05 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,86 @@
-// --------------------------
-// @author Bilal Cinarli
-// --------------------------
-
 'use strict';
 
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  rename = require('gulp-rename');
+var gulp = require('gulp');
 
-var tasks = {
-  sass: function () {
-    return gulp.src('application.scss')
-      .pipe(sass({
-        outputStyle: 'expanded'
-      }))
-      .pipe(rename('rocket.css'))
-      .pipe(gulp.dest('.'));
-  }
-};
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
 
-gulp.task('sass', tasks.sass);
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+var browserSync = require('browser-sync');
+var historyApiFallback = require('connect-history-api-fallback');
+var reload = browserSync.reload;
 
-gulp.task('styles', ['sass'], function () {
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+  this.emit('end');
+}
 
+gulp.task('browser-sync', function() {
+  browserSync({
+    server : {},
+    middleware : [ historyApiFallback() ],
+    ghostMode: false
+  });
 });
 
-gulp.task('watch', ['styles'], function () {
-  // --------------------------
-  // watch:sass
-  // --------------------------
-  gulp.watch('**/*.scss', ['styles']);
+gulp.task('scss',function() {
+  gulp.src('scss/app.scss')
+    .pipe(sass({outputStyle: 'expanded'}).on('error', handleErrors))
+    // .pipe(rename('bundle.css'))
+    // start minify
+    .pipe(sass({outputStyle: 'compressed'}).on('error', handleErrors))
+    .pipe(rename('bundle.css'))
+    // end minify
+    .pipe(gulp.dest('css'))
+    .pipe(reload({stream:true}))
+});
+
+function buildScript(file, watch) {
+  var props = {
+    entries: ['./scripts/' + file],
+    debug : true,
+    transform:  [babelify.configure({stage : 0 })]
+  };
+  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream
+      .on('error', handleErrors)
+      .pipe(source(file))
+      // .pipe(rename('bundle.js'))
+      // start minify
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(rename('bundle.js'))
+      // end minify
+      .pipe(gulp.dest('js'))
+      .pipe(reload({stream:true}))
+  }
+  bundler.on('update', function() {
+    rebundle();
+    gutil.log('Rebundle...');
+  });
+  return rebundle();
+}
+
+gulp.task('scripts', function() {
+  return buildScript('app.js', false);
+});
+
+gulp.task('watch', ['browser-sync', 'scss', 'scripts'], function () {
+  gulp.watch('scss/**/*', ['scss']);
+  return buildScript('app.js', true);
 });
 
 gulp.task('default', ['watch']);
---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+ReactDOM.render(<HelloMessage name="dnomak" />, document.querySelector('#app'));
